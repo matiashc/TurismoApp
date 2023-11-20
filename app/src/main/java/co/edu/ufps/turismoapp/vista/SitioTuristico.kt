0e94be1b2913732a9ecc1ecb13fa50ead11cebdd
matiashc <casanueva1>
2023-11-20 09:05:02 -0500

    20/11/2023-10:01

--- app/src/main/java/co/edu/ufps/turismoapp/vista/SitioTuristico.kt
+++ app/src/main/java/co/edu/ufps/turismoapp/vista/SitioTuristico.kt
@@ -1,6 +1,7 @@
 package co.edu.ufps.turismoapp.vista
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,11 @@ import androidx.recyclerview.widget.RecyclerView
 import co.edu.ufps.turismoapp.R
 import co.edu.ufps.turismoapp.contoller.SitioAdapter
 import co.edu.ufps.turismoapp.modelo.Sitio
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +30,7 @@ private const val ARG_PARAM2 = "param2"
 class SitioTuristico : Fragment() {
     lateinit var contenedor: RecyclerView
     lateinit var sitioAdapter: SitioAdapter
+    val TAG: String = "SitioTuristico"
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -47,7 +54,7 @@ class SitioTuristico : Fragment() {
         val linearLayout = LinearLayoutManager(context)
         linearLayout.orientation = LinearLayoutManager.VERTICAL
         contenedor.layoutManager=linearLayout
-        sitioAdapter = SitioAdapter(context,cargarDatos(),R.id.card)
+        sitioAdapter = SitioAdapter(context,cargarDatosFireBase(),R.id.card)
         contenedor.adapter = sitioAdapter
         return view
     }
@@ -60,6 +67,31 @@ class SitioTuristico : Fragment() {
         sitios.add(Sitio("XER","SITIO 3","SITIO 3 TURISTICO","https://cdn.inteligenciaviajera.com/wp-content/uploads/2020/04/sitios-turisticos-2.jpg",0.0,0.0))
         return sitios
 
+    }
+
+    fun cargarDatosFireBase(): ArrayList<Sitio>{
+        val database = Firebase.database
+        val myRef = database.getReference()
+        val sitios: ArrayList<Sitio> = java.util.ArrayList<Sitio>()
+        // Read from the database
+        myRef.child("sitios").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()){
+                    sitios.clear()
+                    for(data in dataSnapshot.children){
+                        val sitio = data.getValue(Sitio::class.java)
+                        sitios.add(sitio as Sitio)
+                        sitioAdapter.notifyDataSetChanged()
+                    }
+                }
+                Log.d(TAG, "Value se cargo")
+            }
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+        return sitios
     }
 
     companion object {
